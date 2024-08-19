4af3a81163c8b047274ceef50368739dd23afeec
rupesh8690 <rupeshlove8690@gmail.com>
2024-08-19 12:14:27 +0530

    cute dog pics

--- script1.js
+++ script1.js
@@ -1,15 +1,29 @@
-let p=document.querySelector("#para");
-let btn=document.querySelector("button");
-btn.addEventListener("click",getFacts);
-let url="https://catfact.ninja/fact";
-async function getFacts(){
-  try{
-    let res1= await axios.get(url);
-    p.innerText=res1.data.fact;
+// let p=document.querySelector("#para");
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",getFacts);
+// let url="https://catfact.ninja/fact";
+// async function getFacts(){
+//   try{
+//     let res1= await axios.get(url);
+//     p.innerText=res1.data.fact;
     
-    console.log(res1.data.fact);
-  }
-  catch(e){
-    console.log(e);
-  }
+//     console.log(res1.data.fact);
+//   }
+//   catch(e){
+//     console.log(e);
+//   }
+// }
+let url= "https://dog.ceo/api/breeds/image/random";
+let dog_img=document.querySelector(".dog_img");
+let btn=document.querySelector("button");
+btn.addEventListener("click",getDogPic);
+async function getDogPic(){
+    try{
+        let res= await axios.get(url);
+        console.log(res.data.message);
+        dog_img.setAttribute("src",res.data.message);
+    }
+    catch(err){
+        console.log("failed to get url");
+    }
 }
